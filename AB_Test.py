9d8f8b8fbb96f0fd6e60b765085da97dda7220ad
Ömer Ali Uygun <112541841+omerali1996@users.noreply.github.com>
2022-11-12 16:22:08 +0300

    Update AB_Test.py

--- AB_Test.py
+++ AB_Test.py
@@ -32,21 +32,22 @@ df_test.head()
 df_control.head()
 
 def Analysis(df1,df2):
-    print(5*"*"+"DF1 Verisinin Analizi"+"*"*5)
-    print("\nGENEL BİLGİ")
+    print(5*"*"+"DF1 Data Analysis"+"*"*5)
+    print("\nGENERAL INFORMATION")
     print(f"\n{df1.info()}")
     print(f"\n\nDESCRİBE \n{df1.describe().T}")
-    print(f"\n\nSÜTUNLAR \n{[col for col in df1.columns]}")
+    print(f"\n\nCOLUMNS \n{[col for col in df1.columns]}")
     print(f"\n\nSHAPE\n{df1.shape}")
-    print(f"\n\nBOŞ DEĞER\n{df1.isnull().sum()}\n\n")
+    print(f"\n\nNULL VALUES\n{df1.isnull().sum()}\n\n")
 
-    print(5*"*"+"DF2 Verisinin Analizi"+"*"*5)
-    print("\nGENEL BİLGİ")
-    print(f"\n{df2.info()}")
-    print(f"\n\nDESCRİBE \n{df2.describe().T}")
-    print(f"\n\nSÜTUNLAR \n{[col for col in df2.columns]}")
-    print(f"\n\nSHAPE\n{df2.shape}")
-    print(f"\n\nBOŞ DEĞER\n{df2.isnull().sum()}\n\n")
+
+    print(5*"*"+"DF2 Data Analysis"+"*"*5)
+    print("\nGENERAL INFORMATION")
+    print(f"\n{df1.info()}")
+    print(f"\n\nDESCRİBE \n{df1.describe().T}")
+    print(f"\n\nCOLUMNS \n{[col for col in df1.columns]}")
+    print(f"\n\nSHAPE\n{df1.shape}")
+    print(f"\n\nNULL VALUES\n{df1.isnull().sum()}\n\n")
 
 ##############################################################
 
@@ -77,29 +78,26 @@ cont_ort=round(all_df['Purchase_Control'].mean(),5)
 #Görev3
 
 """
-Normallik Varsayımı :
-H0: Normal dağılım varsayımı sağlanmaktadır.
-H1: Normal dağılım varsayımı sağlanmamaktadır.
+Normal Distribution :
+H0: Normal Distribution  OK .
+H1: Normal Distribution not OK .
 
-p < 0.05 H0 RED , p > 0.05 H0 REDDEDİLEMEZ
-Test sonucuna göre normallik varsayımı kontrol ve 
-test grupları için sağlanıyor mu ?
-Elde edilen p-value değerlerini yorumlayınız. 
+p < 0.05 H0 REJECTED , p > 0.05 H0 DO NOT REJECTED
 """
 
-#Normal Dağılım
+#Normal Distribution
 
 p_value_control = shapiro(all_df["Purchase_Control"])[1]
 p_value_test = shapiro(all_df["Purchase_Test"])[1]
 
 
-#Varyans Homojenliği
+#Variance Homogeneity
 
 p_value_levene = levene(all_df['Purchase_Control'],
                         all_df['Purchase_Test'])[1]
 
 
-#Sonuç
+#Result
 
 p_value_ttest = ttest_ind(all_df['Purchase_Control'],
                           all_df['Purchase_Test'],
@@ -108,29 +106,29 @@ p_value_ttest = ttest_ind(all_df['Purchase_Control'],
 
 
 
-########################## Fonksiyonlaştırma
+############# All_Step_Function ###########
 
 def AB_Test(df1, df2, pthres=0.05):
     Analysis(df1, df2)
 
-    print(5 * "*" + "VERİ BİRLEŞTİRME" + 5 * "*"+"\n")
+    print(5 * "*" + "Combining Data" + 5 * "*"+"\n")
 
     df1.columns = (col + "_Test" for col in Test.columns)
     df2.columns = (col + "_Control" for col in Control.columns)
 
     all_df = pd.concat([df1, df2], axis=1)
     print(all_df.head(3))
-    print("\n\n"+5 * "*" + "İLK İNCELEME" + 5 * "*"+"\n")
+    print("\n\n"+5 * "*" + "First Look " + 5 * "*"+"\n")
 
 
     test_ort = round(all_df['Purchase_Test'].mean(),5)
     cont_ort = round(all_df['Purchase_Control'].mean(), 5)
-    print(f"Test Değişkenin Kazanç ortalaması => "
+    print(f"Purchase Average of Test_Value => "
           f"{test_ort}")
-    print(f"Control Değişkenin Kazanç ortalaması => "
+    print(f"Purchase Average of Control_Value => "
           f"{cont_ort}\n\n")
 
-    print(5 * "*" + "NORMAL DAĞILIM İNCELEME" + 5 * "*"+"\n")
+    print(5 * "*" + "Normal Distribution Analysis" + 5 * "*"+"\n")
 
     p_value_control = shapiro(all_df['Purchase_Control'])[1]
     p_value_test = shapiro(all_df['Purchase_Test'])[1]
@@ -138,13 +136,13 @@ def AB_Test(df1, df2, pthres=0.05):
     print(f"p_value_test =>{p_value_test}\n")
 
     if (p_value_test > pthres) & (p_value_control > pthres):
-        print("Normallik H0 Hipotezi Reddedilemez.\n\n")
+        print("HO DO NOT REJECTED\n\n")
     else:
-        print("Veriyi incele !!\n\n")
+        print("Review Dataset !!\n\n")
 
 
 
-    print(5 * "*" + "VARYANS HOMOJENLİĞİ İNCELEME" + 5 * "*"+"\n")
+    print(5 * "*" + "Varience Homogeneity Analysis" + 5 * "*"+"\n")
 
     p_value_levene = levene(all_df['Purchase_Control'],
                             all_df['Purchase_Test'])[1]
@@ -152,27 +150,27 @@ def AB_Test(df1, df2, pthres=0.05):
     print(f"p_value_levene =>{p_value_levene}\n")
 
     if p_value_levene > pthres:
-        print("Varyans Homojenliği H0 Hipotezi Reddedilemez.\n")
+        print("VARIENCE HOMOGENEITY HO DO NOT REJECTED.\n")
 
     else:
-        print("Veriyi incele !!\n")
+        print("Review Dataset !!\n")
 
     if ((p_value_test > pthres) &
         (p_value_control > pthres)) & \
             (p_value_levene > pthres):
-        print("Parametrik Test yapılmalıdır.\n\n"
-              "Açıklama:\n'test_index' fonksiyonunda"
-              "'equal_var = True' yapılmalıdır.\n\n")
+        print("Parametric Test Be Done.\n\n"
+              "Explanation:\n'equal_var = True' applied on "
+              "'test_index' func.\n\n")
     elif ((p_value_test > pthres) &
           (p_value_control > pthres)) & \
             (p_value_levene < pthres):
-        print("Parametrik Test yapılmalıdır.\n "
-              "Açıklama:ttest_index fonksiyonunda,\n"
-              "'equal_var = False' yapılmalıdır.\n\n")
+        print("Parametric Test Be Done.\n\n"
+              "Explanation:\n'equal_var = False' applied on "
+              "'test_index' func.\n\n")
     else:
-        print("Non-Parametrik Test yapılmalıdır.\n\n")
+        print("Non-Parametric Test Be Done.\n\n")
 
-    print(5 * "*" + "SONUÇ" + 5 * "*"+"\n")
+    print(5 * "*" + "RESULT" + 5 * "*"+"\n")
     "\n"
     p_value_ttest = ttest_ind(all_df['Purchase_Control'],
                               all_df['Purchase_Test'],
@@ -181,12 +179,12 @@ def AB_Test(df1, df2, pthres=0.05):
     print(f"p_value_ttest =>{p_value_ttest}\n")
 
     if p_value_ttest > pthres:
-        print(f"İlk Ortalamalar \nTest için =>{test_ort},  Kontrol için =>{cont_ort}\nolmasına rağmen,"
-              f"ortalamalar birbirine eşittir.\nFark olması istatistiksel olarak şanstır.\n"
-              f"p_value_ttest değeri {round(p_value_ttest, 5)} çıkmıştır.")
+        print(f"Although First Averages \nTest_Value  =>{test_ort},  Control_Value =>{cont_ort}\n,"
+              f"averages are same statisticaly.\nIt is coincidence that Averages are different before analysis.\n"
+              f"p_value_ttest_value is {round(p_value_ttest, 5)}.")
 
     else:
-        print(f"p_value_ttest değeri {pthres}'ten küçük")
+        print(f"p_value_ttest_value is lower than {pthres}")
 
 AB_Test(df_test,df_control)
 
